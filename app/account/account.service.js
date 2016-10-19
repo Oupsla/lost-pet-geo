74f9ac6dc448b7b4c1f3c99b9e8fdc2ee731f840
Marine Bal <marine.bal.1993@gmail.com>
2016-10-19 11:31:38 +0200

    use api

--- app/account/account.service.js
+++ app/account/account.service.js
@@ -3,34 +3,22 @@
     .module('account')
     .service('AccountService', accountService);
 
-  accountService.$inject = ['$q'];
-  function accountService($q) {
+  accountService.$inject = ['$q', '$http'];
+  function accountService($q, $http) {
     var self = this;
 
     self.getAccount = function (id) {
-      return $q((resolve, reject) => {
-        return resolve({
-          id: id,
-          name: 'marine',
-          firstName: 'bal',
-          email: 'ooo-mama-ooo@hotmail.fr',
-          phone: '0668557173',
-          birthdate: new Date('17-04-1993'),
-          address: {
-            number: 314,
-            street: 'rue truc',
-            postalCode: 59100,
-            city: 'roubaix',
-            country: 'france'
-          }
-        });
-      });
+      return $http.get("http://lostpet-api.mybluemix.net/api/v1.0/users/" + id)
+        .then((resp) => resp.data);
     };
 
     self.updateAccount = function (account) {
-      return $q((resolve, reject) => {
-        return resolve({});
-      });
+      return $http.post(
+        "http://lostpet-api.mybluemix.net/api/v1.0/users/",
+        {
+          account
+        })
+        .then((resp) => resp.data);
     };
   }
 })();
--- app/list/pet/list-pet.service.js
+++ app/list/pet/list-pet.service.js
@@ -8,32 +8,8 @@
     var self = this;
 
     self.getListPet = function (accountId) {
-      return $q((resolve, reject) => {
-        return resolve([
-          {
-            accountId: accountId,
-            id: 1,
-            photo: 'http://www.apagi.fr/media/filer_public/37/85/3785774d-1d65-4a7c-8f44-e6175f92a603/jumper-chien-male-yorkshire-noir-et-feu-1.jpg',
-            pet: {
-              type: 'chien',
-              name: 'toutou',
-              race: 'bichon',
-              color: 'blanc',
-            }
-          },
-          {
-            accountId: accountId,
-            id: 2,
-            photo: 'http://previews.123rf.com/images/bartkowski/bartkowski1203/bartkowski120300005/12612383-Noir-petit-chaton-assis-un-sur-un-fond-blanc-Banque-d\'images.jpg',
-            pet: {
-              type: 'chat',
-              name: 'chaton',
-              race: '',
-              color: 'noir'
-            }
-          }
-        ]);
-      });
+      return $http.get("http://lostpet-api.mybluemix.net/api/v1.0/pets/")
+        .then((resp) => resp.data);
     }
   }
 })();
